08d8d89b0781e5994c3f8b99cd9132be94419761
emberrap <emberrap@students.zhaw.ch>
2016-10-25 22:25:24 +0200

    Added the C# project

---
+++ YTcore/YTcore/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The core containing all the functionality of the library.
+/// </summary>
+namespace YTcore
+{
+    public class Class1
+    {
+    }
+}
