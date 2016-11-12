18240c20f08cb5806ba972aab5c7c0e8ceb28aa4
emberrap <emberrap@students.zhaw.ch>
2016-11-12 23:52:18 +0100

    Extended the InternetSite class
    Not yet done with the request and response handling

--- youtuber/youtuber/net/InternetSite.cs
+++ youtuber/youtuber/net/InternetSite.cs
@@ -15,15 +15,63 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace youtuber.net
 {
     /// <summary>
-    /// Contains the most basic methods for interacting with an internet site.
+    ///     Contains the most basic methods for interacting with an internet site.
     /// </summary>
-    public abstract class InternetSite
+    public class InternetSite
     {
+        protected HttpWebResponse response;
 
+        public InternetSite(string content){
+            Content = content;
+        }
+
+        public string Content {get; private set;}
+
+        public static InternetSite FromContent(string content){
+            return new InternetSite(content);
+        }
+
+        public static async Task<InternetSite> FromResponse(HttpWebResponse response){
+            var respStream = response.GetResponseStream();
+            if (respStream == null) {
+                throw new WebException("No response stream recieved.");
+            }
+            var content = string.Empty;
+            using (var strmReader = new StreamReader(respStream)) {
+                content = await strmReader.ReadToEndAsync();
+                strmReader.DiscardBufferedData();
+            }
+            var site = FromContent(content);
+            site.response = response;
+            return site;
+        }
+
+        protected static async Task<T> Load<T>(string url, string method = Http.Get) where T : InternetSite{
+            var req = WebRequest.CreateHttp(url);
+            req.Method = method;
+            req.KeepAlive = true;
+            return await Load<T>(req);
+        }
+
+        protected static async Task<T> Load<T>(HttpWebRequest request) where T : InternetSite{
+            var resp = await request.GetResponseAsync();
+            var respStream = resp.GetResponseStream();
+            if (respStream == null) {
+                throw new WebException("No response stream recieved.");
+            }
+            var content = string.Empty;
+            using (var strmReader = new StreamReader(respStream)) {
+                content = await strmReader.ReadToEndAsync();
+                strmReader.DiscardBufferedData();
+            }
+            return (T) FromContent(content);
+        }
     }
 }
